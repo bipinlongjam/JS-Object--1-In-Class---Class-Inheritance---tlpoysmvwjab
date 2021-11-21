a1289226a20e28e46b1f12288b545469f707f08d
bipinlongjam <41874001+bipinlongjam@users.noreply.github.com>
2021-11-21 09:12:29 +0530

    Add files via upload

---
+++ src/circle.js
@@ -0,0 +1,13 @@
+// declare class
+class Circle extends Shape {
+  super(radius) {
+    this.radius = radius;
+  }
+
+  calculateArea() {
+    console.log("Area");
+  }
+}
+
+// export class using module.exports
+module.exports = Circle;
---
+++ src/shape.js
@@ -0,0 +1,15 @@
+//declare class
+class Shape {
+  constructor(color) {
+    this.color = "color";
+  }
+
+  drawShape() {
+    console.log("Shape");
+  }
+  calculateArea() {
+    console.log("Area");
+  }
+}
+//export class using module.exports
+module.exports = Shape;
